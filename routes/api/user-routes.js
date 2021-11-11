883f0d2f211b9775614f117af4ed09d0a35bf513
Noah Tidwell <ntidwell34@yahoo.com>
2021-11-11 14:07:36 -0600

    set up bcrypt for login credentials

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -47,6 +47,33 @@ router.post('/', (req, res) => {
     });
 });
 
+router.post('/login', (req, res) => {
+// expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+  
+// Query operation
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  })
+  .then(dbUserData => {
+    if(!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address! Try again.'});
+      return;
+    }
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if(!validPassword) {
+      res.status(400).json({ message: 'Incorrect Password!'});
+      return;
+    }
+    res.json({ user: dbUserData, messgae: 'Login Successful!'});
+
+    //verify user
+  })
+
+
+});
+
 router.put('/:id', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
